89fcf786b0460d819668400f1663e69989839ec3
AntScript <antscript@gmail.com>
2016-10-26 16:46:22 +0800

    no message

---
+++ Sources/macOS/OSTest.swift
@@ -0,0 +1,15 @@
+//
+//  OSTest.swift
+//  ASKit
+//
+//  Created by AntScript on 26/10/2016.
+//
+//
+
+import Foundation
+
+class OSTest {
+    static public func test() -> String {
+        return "macOS"
+    }
+}
