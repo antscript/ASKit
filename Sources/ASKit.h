bc1f8d936fdb76cf691e88b02894d8477a8fca7a
AntScript <antscript@gmail.com>
2016-10-26 15:27:13 +0800

    no message

---
+++ Sources/ASKit.h
@@ -0,0 +1,19 @@
+//
+//  ASKit iOS.h
+//  ASKit iOS
+//
+//  Created by AntScript on 26/10/2016.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ASKit iOS.
+FOUNDATION_EXPORT double ASKitVersionNumber;
+
+//! Project version string for ASKit iOS.
+FOUNDATION_EXPORT const unsigned char ASKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ASKit_iOS/PublicHeader.h>
+
+
---
+++ Sources/MyTest.swift
@@ -0,0 +1,19 @@
+//
+//  MyTest.swift
+//  ASKit
+//
+//  Created by AntScript on 26/10/2016.
+//
+//
+
+
+
+public class MyTest {
+    
+    static public func test() -> String {
+        return "hello world"
+    }
+    
+    
+    
+}
